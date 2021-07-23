c0475c95f321eec7f2293c95a1dc8fc8629f2e03
ep <eugen.pt@gmail.com>
2021-07-23 22:54:11 +0700

    multiline plugin that wont work.. heh)

--- init.lua
+++ init.lua
@@ -1,16 +1,17 @@
+-- mod-version:1 -- lite-xl 1.16
 local core = require "core"
 local common = require "core.common"
 local config = require "core.config"
 local style = require "core.style"
 
-local misc = require "lite-xl-vibe.misc"
+local misc = require "misc"
 
 local vibe = {}
 
 vibe.mode = 'insert'
 
 
-core.error(vibe.mode:isUpperCase())
+core.error(vibe.mode:isUpperCase() and "true" or "false")
 
 core.log("lite-xl-vibe loaded.")
 return vibe
