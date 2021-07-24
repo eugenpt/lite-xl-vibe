124a33ee6edb6b4dba248198a849f33463e45760
ep <eugen.pt@gmail.com>
2021-07-24 21:08:29 +0700

    seems fixed, but looks weird

--- init.lua
+++ init.lua
@@ -4,13 +4,13 @@ local common = require "core.common"
 local config = require "core.config"
 local style = require "core.style"
 
-local misc = require "misc"
+local misc = require "plugins.lite-xl-vibe.misc"
 
 local vibe = {}
 
 vibe.mode = 'insert'
 
-
+-- yeah, this is a test
 core.error(vibe.mode:isUpperCase() and "true" or "false")
 
 core.log("lite-xl-vibe loaded.")
