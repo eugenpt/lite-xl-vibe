98f7b8d735966eca449631194557d9305e0d5360
ep <eugen.pt@gmail.com>
2021-07-24 21:31:50 +0700

    starting with some minor things

--- misc.lua
+++ misc.lua
@@ -1,3 +1,17 @@
+--[[
+
+This is a miscellaneous module for lite-xl-vibe
+(duh)
+
+main intentions are
+
+- to extend lite-xl (or lua) classes with necessary methods
+
+- to add some minor things I personally find useful
+
+- to dump things here if I don't have any other submodule for them
+
+]]--
 local core = require "core"
 local command = require "core.command"
 local keymap = require "core.keymap"
@@ -8,12 +22,15 @@ local config = require "core.config"
 local common = require "core.common"
 local translate = require "core.doc.translate"
 
+local misc = {}
 
 function string:isUpperCase()
   return self:upper()==self and self:lower()~=self
 end
 
 function string:find_literal(substr)
+  -- literal find, instead of pattern-based 
+  --  lua may have it, but I am not aware of it
   for j=1, (#self - #substr + 1) do
     if self:sub(j, j + #substr - 1) == substr then
       return j
@@ -29,5 +46,64 @@ function string:isNumber()
       return false
     end
   end
-  return false
+  return true
 end
+
+
+local function dv()
+  return core.active_view
+end
+
+local function doc()
+  return core.active_view.doc
+end
+
+function misc.move_to_line(line)
+  doc():move_to(function() return tonumber(num_arg),0 end, dv())
+end
+
+function misc.append_line_if_last_line(line)
+  if line >= #doc().lines then
+    doc():insert(line, math.huge, "\n")
+  end
+end
+
+-------------------------------------------------------------------------------
+-- commands
+-------------------------------------------------------------------------------
+
+command.add(nil, {
+  -- I find this sort of useful for debugging
+  --   (it may be already present in lite/lite-xl,
+  --      but it was easier for me to just write it )
+  ["core:exec-selection"] = function()
+    local text = doc():get_text(doc():get_selection())
+    if doc():has_selection() then
+      text = doc():get_text(doc():get_selection())
+    else
+      local line, col = doc():get_selection()
+      doc():move_to(translate.start_of_line, dv())
+--      doc():move_to(translate.next_word_start, dv())
+      doc():select_to(translate.end_of_line, dv())
+      if doc():has_selection() then
+        text = doc():get_text(doc():get_selection())
+      else
+        return nil  
+      end
+      doc():move_to(function() return line, col end, dv())
+    end
+    assert(load(text))()
+  end,
+  -- after some thoughts this was even more useful
+  --  ( I mean you do need to `require` a lot of stuff.. )
+  ["core:exec-file"] = function()
+    assert(load(table.concat(doc().lines)))()
+  end,
+  
+  -- yeah, I do like vim
+  ["so % core:exec-file"] = function()
+    command.perform("core:exec-file")
+  end,
+})
+
+return misc
