fefa65c9696b93e19f262e0eea3c8c7bb72afd71
ep <eugen.pt@gmail.com>
2021-08-08 13:37:07 +0700

    workspace management here, huh?

--- init.lua
+++ init.lua
@@ -40,6 +40,8 @@ require "plugins.lite-xl-vibe.FileView"
 
 require "plugins.lite-xl-vibe.visual_mode"
 
+require "plugins.lite-xl-vibe.vibeworkspace"
+
 local function dv()
   return core.active_view
 end
