64f7d334e712cf30443fc84296fc5eb77cbf3f3b
Martino Ferrari <martinogiordano.ferrari@iter.org>
2023-10-27 13:37:52 +0200

    fix: simple workaround to error that disable custom colors

--- interface.lua
+++ interface.lua
@@ -2,7 +2,8 @@
 
 All things interface
 
-]]--
+]]
+   --
 
 local core = require "core"
 local command = require "core.command"
@@ -35,11 +36,11 @@ function StatusView:get_items()
     local indent_size = indent and tostring(indent.size) .. (indent.confirmed and "" or "*") or "unknown"
 
     local line_perc_str = '???'
-    pcall(function() 
+    pcall(function()
       line_perc_str = string.format("% 3d%%", line / #dv.doc.lines * 100)
     end)
-    
-    
+
+
 
     return {
       dirty and style.accent or style.text, style.icon_font, "f",
@@ -49,21 +50,21 @@ function StatusView:get_items()
       dv.doc.filename and style.text or style.dim, dv.doc:get_name(),
       style.text, style.code_font,
       self.separator2,
-      "L", string.format('% 4d',line), " :",
-      col > config.line_limit and style.accent or style.text, string.format('% 3d',col), " C",
+      "L", string.format('% 4d', line), " :",
+      col > config.line_limit and style.accent or style.text, string.format('% 3d', col), " C",
       style.text,
       " ", -- self.separator,
       line_perc_str,
       self.separator2,
       core.vibe.stroke_seq,
       self.separator2,
-     (core.vibe.debug_str and (#core.vibe.debug_str > config.vibe.debug_str_max ))
-        and (core.vibe.debug_str:sub(1, math.floor(config.vibe.debug_str_max/2))
-              .. core.vibe.debug_str:sub(#core.vibe.debug_str - math.ceil(config.vibe.debug_str_max/2)
-                                        ,#core.vibe.debug_str))
-        or core.vibe.debug_str,
-      (config.vibe.permanent_status_tooltip 
-        and self.separator2 .. config.vibe.permanent_status_tooltip  or '')
+      (core.vibe.debug_str and (#core.vibe.debug_str > config.vibe.debug_str_max))
+      and (core.vibe.debug_str:sub(1, math.floor(config.vibe.debug_str_max / 2))
+        .. core.vibe.debug_str:sub(#core.vibe.debug_str - math.ceil(config.vibe.debug_str_max / 2)
+        , #core.vibe.debug_str))
+      or core.vibe.debug_str,
+      (config.vibe.permanent_status_tooltip
+        and self.separator2 .. config.vibe.permanent_status_tooltip or '')
     }, {
       style.dim, self.separator2, style.text, indent_label, indent_size,
       style.dim, self.separator2, style.text,
@@ -73,17 +74,17 @@ function StatusView:get_items()
       self.separator,
       style.code_font,
       dv.doc.crlf and "CRLF" or "  LF",
-      style.text, ' |', string.format('#%3s',core.vibe.num_arg),
-      style.text, '|', string.format("%7s",core.vibe.last_stroke), 
+      style.text, ' |', string.format('#%3s', core.vibe.num_arg),
+      style.text, '|', string.format("%7s", core.vibe.last_stroke),
     }
   end
 
   return {
-      style.text, style.icon_font, "P",
-      style.code_font, style.text, self.separator2,
-      style.accent, get_mode_str(), style.text, self.separator2,
-      style.dim, style.font, style.text,
-    style.text, 
+    style.text, style.icon_font, "P",
+    style.code_font, style.text, self.separator2,
+    style.accent, get_mode_str(), style.text, self.separator2,
+    style.dim, style.font, style.text,
+    style.text,
     style.font,
     core.vibe.debug_str,
   }, {
@@ -91,21 +92,20 @@ function StatusView:get_items()
     style.font, style.dim, self.separator2,
     style.text, self.separator2, #core.docs, " / ",
     #core.project_files, " files",
-      style.code_font,
-      style.text, ' |', string.format('#%3s',core.vibe.num_arg),
-      style.text, '|', string.format("%7s",core.vibe.last_stroke), 
+    style.code_font,
+    style.text, ' |', string.format('#%3s', core.vibe.num_arg),
+    style.text, '|', string.format("%7s", core.vibe.last_stroke),
   }
 end
 
 local function draw_items(self, items, x, y, draw_fn)
   local font = style.font
   local color = style.text
-
   for _, item in ipairs(items) do
     if type(item) == "userdata" then
       font = item
     elseif type(item) == "table" then
-      color = item
+      -- color = item
     else
       x = draw_fn(font, color, item, nil, x, y, 0, self.size.y)
     end
@@ -124,11 +124,11 @@ function StatusView:draw_items(items, right_align, yoffset)
   local x, y = self:get_content_offset()
   y = y + (yoffset or 0)
   if right_align then
-    local w = draw_items(self, items, 0, 0, text_width)    
+    local w = draw_items(self, items, 0, 0, text_width)
     x = x + self.size.x - w - style.padding.x
-    
+
     renderer.draw_rect(x, y, self.size.x, self.size.y + y % 1, style.background2)
-    
+
     -- this return is what's changed
     draw_items(self, items, x, y, common.draw_text)
   else
@@ -136,7 +136,6 @@ function StatusView:draw_items(items, right_align, yoffset)
     draw_items(self, items, x, y, common.draw_text)
   end
 end
-
 
 function StatusView:draw()
   self:draw_background(style.background2)
@@ -160,11 +159,11 @@ end
 
 status.caret_width__orig = style.caret_width
 
-command.add_hook("vibe:switch-to-insert-mode", { function() 
+command.add_hook("vibe:switch-to-insert-mode", { function()
   style.caret_width = status.caret_width__orig
 end })
 
-command.add_hook("vibe:switch-to-normal-mode", { function() 
+command.add_hook("vibe:switch-to-normal-mode", { function()
   if core.active_view.get_font then
     style.caret_width = core.active_view:get_font():get_width(' ')
   end
@@ -172,12 +171,11 @@ end })
 
 local DocView__update__orig = DocView.update
 function DocView:update(...)
-  style.caret_width = (core.vibe.mode=='normal' and core.active_view.get_font)
-                      and core.active_view:get_font():get_width(' ')
-                      or status.caret_width__orig
-  DocView__update__orig(self,...)
+  style.caret_width = (core.vibe.mode == 'normal' and core.active_view.get_font)
+      and core.active_view:get_font():get_width(' ')
+      or status.caret_width__orig
+  DocView__update__orig(self, ...)
 end
-
 
 core.log('interface loaded?')
 
@@ -200,20 +198,24 @@ local function draw_text(x, y, color)
   x = x + style.padding.x
   renderer.draw_rect(x, y, math.ceil(1 * SCALE), dh, color)
   local lines = {
-    { fmt = "%s to run a command", cmd = "core:find-command" },
-    { fmt = "%s to open a file from the project", cmd = "core:find-file" },
-    { fmt = "%s to change project folder", cmd = "core:change-project-folder" },
-    { fmt = "%s to open a project folder", cmd = "core:open-project-folder" },
+    { fmt = "%s to run a command",                                                                             cmd =
+    "core:find-command" },
+    { fmt = "%s to open a file from the project",                                                              cmd =
+    "core:find-file" },
+    { fmt = "%s to change project folder",                                                                     cmd =
+    "core:change-project-folder" },
+    { fmt = "%s to open a project folder",                                                                     cmd =
+    "core:open-project-folder" },
     { text = " " },
     { text = "Ctrl+N/P selects next/previous of the suggestions" },
     { text = " " },
-    { text = "you are in "..get_mode_str().." mode now" },
-    { text = "Escape / Ctrl+[ to enter NORMAL mode as in VIM"  },
-    { text = "while in NORMAL mode, use <i> to enter INSERT mode again"  },
+    { text = "you are in " .. get_mode_str() .. " mode now" },
+    { text = "Escape / Ctrl+[ to enter NORMAL mode as in VIM" },
+    { text = "while in NORMAL mode, use <i> to enter INSERT mode again" },
     { text = " " },
     { text = "Press Alt+h to show/scroll stroke suggestions" },
     { text = " " },
-    { text = "A good place to start is to press " .. (core.vibe.mode=="normal" and "" or "<ESC>") .. "<space>" },
+    { text = "A good place to start is to press " .. (core.vibe.mode == "normal" and "" or "<ESC>") .. "<space>" },
     { text = " .. and move the mouse a bit, to force drawing of the tooltip .." },
   }
   th = style.font:get_height()
@@ -245,7 +247,6 @@ function EmptyView:draw()
   draw_text(x, y, style.dim)
 end
 
-  
 -------------------------------------------------------------------------------
 
 return status
